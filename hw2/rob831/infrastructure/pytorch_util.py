d8821c2ed8d2660e81947cffba117c8fdb4bac40
liver121888 <liver121888@gmail.com>
2025-02-22 12:33:22 -0500

    Update hw2 with hw1 solution

--- hw2/rob831/infrastructure/pytorch_util.py
+++ hw2/rob831/infrastructure/pytorch_util.py
@@ -47,8 +47,14 @@ def build_mlp(
         activation = _str_to_activation[activation]
     if isinstance(output_activation, str):
         output_activation = _str_to_activation[output_activation]
-    
-    # TODO: get this from hw1
+    layers = []
+    in_size = input_size
+    for _ in range(n_layers):
+        layers.append(nn.Linear(in_size, size))
+        layers.append(activation)
+        in_size = size
+    layers.append(nn.Linear(in_size, output_size))
+    layers.append(output_activation)
     raise NotImplementedError
 
 
