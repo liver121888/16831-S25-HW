e4092d37f270af5ff82d5f7c5b9d7d3df3f75a80
liver121888 <liver121888@gmail.com>
2025-03-28 14:28:45 -0400

    Update hw1 solutions

--- hw4/rob831/hw4_part1/infrastructure/rl_trainer.py
+++ hw4/rob831/hw4_part1/infrastructure/rl_trainer.py
@@ -209,25 +209,15 @@ class RL_Trainer(object):
         print("\nCollecting data to be used for training...")
         paths, envsteps_this_batch = utils.sample_trajectories(
             self.env, collect_policy, num_transitions_to_sample, self.params['ep_len'])
-
+        
+        # collect more rollouts with the same policy, to be saved as videos in tensorboard
         train_video_paths = None
         if self.log_video:
             print('\nCollecting train rollouts to be used for saving videos...')
             train_video_paths = utils.sample_n_trajectories(self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
-
-        return paths, envsteps_this_batch, train_video_paths
-
-
-        # collect more rollouts with the same policy, to be saved as videos in tensorboard
-        train_video_paths = None
-        if self.logvideo:
-            print('\nCollecting train rollouts to be used for saving videos...')
-            train_video_paths = utils.sample_n_trajectories(self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
-
         if save_expert_data_to_disk and itr == 0:
             with open('expert_data_{}.pkl'.format(self.params['env_name']), 'wb') as file:
                 pickle.dump(paths, file)
-
         return paths, envsteps_this_batch, train_video_paths
 
     def train_agent(self):
